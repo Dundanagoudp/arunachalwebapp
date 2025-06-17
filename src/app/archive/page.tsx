9d508321d141d78668e07936a2e474996030346c
chetanv18~ <chetan@gully2global.com>
2025-06-17 17:49:32 +0530

    layout setup nav bar

---
+++ src/app/archive/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function Archive() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+  Archive
+    </div>
+  );
+} 
---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function Schedule() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+    Schedule 
+    </div>
+  );
+} 
