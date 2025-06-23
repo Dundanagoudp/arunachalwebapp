db21d1b80e56fd622483ba39e08b3aadf9a8aea0
chetanv18~ <chetan@gully2global.com>
2025-06-23 11:20:41 +0530

    speaker section ui

--- src/components/DiamondPatternBackground.tsx
+++ src/components/DiamondPatternBackground.tsx
@@ -6,34 +6,34 @@ export default function DiamondPatternBackground() {
   return (
     <div className="absolute inset-0 pointer-events-none">
       {/* Top left diamonds */}
-      <div className="absolute top-8 left-8 w-8 h-8 opacity-30">
+      <div className="absolute top-8 left-8 w-8 h-8 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={32} height={32} className="w-full h-full" />
       </div>
-      <div className="absolute top-20 left-20 w-6 h-6 opacity-20">
+      <div className="absolute top-20 left-20 w-6 h-6 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={24} height={24} className="w-full h-full" />
       </div>
 
       {/* Top right diamonds */}
-      <div className="absolute top-12 right-12 w-10 h-10 opacity-25">
+      <div className="absolute top-12 right-12 w-10 h-10 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={40} height={40} className="w-full h-full" />
       </div>
-      <div className="absolute top-32 right-8 w-7 h-7 opacity-30">
+      <div className="absolute top-32 right-8 w-7 h-7 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={28} height={28} className="w-full h-full" />
       </div>
 
       {/* Middle left diamonds */}
-      <div className="absolute top-1/2 left-4 w-9 h-9 opacity-20 -translate-y-1/2">
+      <div className="absolute top-1/2 left-4 w-9 h-9  -translate-y-1/2">
         <Image src="/gallery/diamond-pattern.png" alt="" width={36} height={36} className="w-full h-full" />
       </div>
-      <div className="absolute top-1/2 left-16 w-5 h-5 opacity-25 translate-y-8">
+      <div className="absolute top-1/2 left-16 w-5 h-5  translate-y-8">
         <Image src="/gallery/diamond-pattern.png" alt="" width={20} height={20} className="w-full h-full" />
       </div>
 
       {/* Middle right diamonds */}
-      <div className="absolute top-1/2 right-6 w-8 h-8 opacity-30 -translate-y-8">
+      <div className="absolute top-1/2 right-6 w-8 h-8  -translate-y-8">
         <Image src="/gallery/diamond-pattern.png" alt="" width={32} height={32} className="w-full h-full" />
       </div>
-      <div className="absolute top-1/2 right-20 w-6 h-6 opacity-20 translate-y-12">
+      <div className="absolute top-1/2 right-20 w-6 h-6  translate-y-12">
         <Image src="/gallery/diamond-pattern.png" alt="" width={24} height={24} className="w-full h-full" />
       </div>
 
@@ -41,18 +41,18 @@ export default function DiamondPatternBackground() {
       <div className="absolute bottom-16 left-12 w-7 h-7 opacity-25">
         <Image src="/gallery/diamond-pattern.png" alt="" width={28} height={28} className="w-full h-full" />
       </div>
-      <div className="absolute bottom-8 right-16 w-9 h-9 opacity-20">
+      <div className="absolute bottom-8 right-16 w-9 h-9 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={36} height={36} className="w-full h-full" />
       </div>
-      <div className="absolute bottom-24 right-4 w-5 h-5 opacity-30">
+      <div className="absolute bottom-24 right-4 w-5 h-5 ">
         <Image src="/gallery/diamond-pattern.png" alt="" width={20} height={20} className="w-full h-full" />
       </div>
 
       {/* Additional scattered diamonds for mobile */}
-      <div className="absolute top-1/4 left-1/4 w-4 h-4 opacity-15 md:hidden">
+      <div className="absolute top-1/4 left-1/4 w-4 h-4  md:hidden">
         <Image src="/gallery/diamond-pattern.png" alt="" width={16} height={16} className="w-full h-full" />
       </div>
-      <div className="absolute bottom-1/3 right-1/3 w-6 h-6 opacity-20 md:hidden">
+      <div className="absolute bottom-1/3 right-1/3 w-6 h-6  md:hidden">
         <Image src="/gallery/diamond-pattern.png" alt="" width={24} height={24} className="w-full h-full" />
       </div>
     </div>
--- src/components/speakers/modules/speakers-topsection.tsx
+++ src/components/speakers/modules/speakers-topsection.tsx
@@ -4,13 +4,12 @@ import Image from "next/image"
 import SunIcon from "@/components/sunicon-gif";
 import dynamic from "next/dynamic";
 
-const DiamondPatternBackground = dynamic(() => import("./DiamondPatternBackground"), { ssr: false });
+const DiamondPatternBackground = dynamic(() => import("../../DiamondPatternBackground"), { ssr: false });
 
 export default function Speakerstopsection() {
   return (
-    <section className="relative min-h-screen w-full overflow-hidden" style={{ backgroundColor: "#fdf8f0" }}>
+    <section className="relative min-h-screen w-full overflow-hidden bg-[#fdf8f0]">
       <DiamondPatternBackground />
-
       {/* Main content */}
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 py-16">
         {/* Title */}
@@ -22,12 +21,12 @@ export default function Speakerstopsection() {
 
         {/* Animated Sun Icon */}
         <div className="mb-8 md:mb-12 lg:mb-16">
-          <SunIcon size={120} className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32" />
+          <SunIcon size={130} className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32" />
         </div>
 
         {/* Text content */}
         <div className="text-center max-w-2xl">
-          <p className="font-bilo text-xl md:text-2xl lg:text-3xl font-semibold leading-relaxed text-[#2D2D2D]">
+          <p className="font-bilo text-xl md:text-2xl lg:text-3xl font-semibold leading-relaxed text-[#000000]">
             From India&apos;s first light,
             <br />
             something bright is coming
