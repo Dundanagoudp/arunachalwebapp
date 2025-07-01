0c469f929489cdc53023c4772a17eaa57e0fe7dd
chetanv18~ <chetan@gully2global.com>
2025-07-01 18:09:56 +0530

    archive page erro fixed

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -46,9 +46,9 @@ export function NavMain({
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
                 <SidebarMenuButton tooltip={item.title}>
-                  {item.icon && <item.icon />}
+                  {item.icon && <item.icon style={{ color: '#1A3FA9' }} />}
                   <span>{item.title}</span>
-                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  <ChevronRight style={{ color: '#1A3FA9' }} className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                 </SidebarMenuButton>
               </CollapsibleTrigger>
               <CollapsibleContent>
