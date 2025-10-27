2e959ab26f169a745d4bf641708fb55b6e1849ae
chetanv18~ <chetan@gully2global.com>
2025-10-27 14:40:55 +0530

    next config Content-Security-Policy added

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,24 @@ const nextConfig: NextConfig = {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://litfest.arunachal.gov.in",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https: blob: https://img.youtube.com",
+              "media-src 'self' https:",
+              "connect-src 'self' https://litfest.arunachal.gov.in",
+              "frame-src 'self' https://www.youtube.com",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+              "frame-ancestors 'none'",
+              "upgrade-insecure-requests"
+            ].join("; ")
+          },
           { key: "X-DNS-Prefetch-Control", value: isDev ? "on" : "off" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
         ],
